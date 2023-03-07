59f16d5845974c352502c91550d6a1eee3c6a63e
chromodorididae <98478977+chromodorididae@users.noreply.github.com>
2023-03-07 16:23:33 -0700

    app imports

--- src/App.js
+++ src/App.js
@@ -3,8 +3,13 @@ import './index.css';
 import Search from "./Search";
 import Newsletter from "./Newsletter";
 // import Navbar from "./Navbar.js";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Link } from "react-router-dom";
+
+// import { Routes, Route } from "react-router-dom"
+import Home from "./Home"
+import About from "./About"
+import Contact from "./Contact"
 
 function App() {
 
